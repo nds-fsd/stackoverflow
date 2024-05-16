be19b45dc12806a4557a40e890abbc31aa9f948b
kovvik23 <08kovacs.v@gmail.com>
2024-05-16 18:35:39 +0200

    Make component naming consistent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './components/HomePage';
-import QuestionForm from './Components/QuestionForm/QuestionForm';
+import QuestionPage from './components/QuestionPage/QuestionPage';
+import QuestionForm from './components/QuestionForm/QuestionForm';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/questions' element={<QuestionPage />} />
         <Route path='/questions/new' element={<QuestionForm />} />
       </Routes>
     </div>
