a994100c9d351b75f3f72483d216d2f007cf31d1
kovvik23 <08kovacs.v@gmail.com>
2024-05-16 18:27:36 +0200

    Rename newQuestion to QuestionForm and separate component to folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './components/HomePage';
-import QuestionPage from './components/QuestionPage/QuestionPage';
-import QuestionForm from './components/newQuestion.jsx';
-
+import QuestionForm from './Components/QuestionForm/QuestionForm';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/Questions' element={<QuestionPage />} />
         <Route path='/questions/new' element={<QuestionForm />} />
       </Routes>
     </div>
