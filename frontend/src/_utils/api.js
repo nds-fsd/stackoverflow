ddc552abd28cdcbb5e74ee9fc5d2e0b80b50ae65
Alex Besse Donato <alexbessedonato03@gmail.com>
2024-06-05 21:03:30 +0200

    solucionado axios

--- frontend/src/_utils/api.js
+++ frontend/src/_utils/api.js
@@ -3,8 +3,9 @@ import { getUserToken } from './localStorage.utils';
 
 export const api = () => {
   const token = getUserToken();
+  console.log('etoy dentro de api', process.env.REACT_APP_BACKEND_URL);
   return axios.create({
-    baseURL: 'process.env.REACT_APP_BACKEND_URL',
+    baseURL: process.env.REACT_APP_BACKEND_URL,
     headers: {
       Authorization: `Bearer ${token}`,
     },
