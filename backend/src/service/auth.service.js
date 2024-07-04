61e84dd8f9ffcc452317eb52ea4bacdc7878c001
kovvik23 <08kovacs.v@gmail.com>
2024-07-04 13:45:08 +0200

    Add jwt middleware and auth service

---
+++ backend/src/service/auth.service.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const generateJWT = (user) => {
+  return jwt.sign(
+    {
+      email: user.email,
+      name: user.name,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: '1d' },
+  );
+};
+
+const validateJWT = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+module.exports = {
+  generateJWT,
+  validateJWT,
+};
