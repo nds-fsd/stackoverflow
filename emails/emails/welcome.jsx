87eacc3bc5e021a4cff513c3f088849a33a7265e
Sofia Simeonoff <sofiasimeonoffcollazo@gmail.com>
2024-07-08 19:04:18 +0200

    welcome email terminado

--- emails/emails/welcome.jsx
+++ emails/emails/welcome.jsx
@@ -1,4 +1,4 @@
-import { Body, Button, Container, Head, Heading, Hr, Html, Preview, Section, Text } from '@react-email/components';
+import { Body, Button, Container, Head, Heading, Hr, Html, Preview, renderWhiteSpace, Section, Text } from '@react-email/components';
 import * as React from 'react';
 
 export const WelcomeEmail = ({ url }) => (
@@ -8,14 +8,32 @@ export const WelcomeEmail = ({ url }) => (
     <Body style={main}>
       <Container style={container}>
         <Section style={box}>
-          <Heading>Welcome to Devvit!</Heading>
+          <Heading style={title}>Welcome to Devvit!</Heading>
           <Hr style={hr} />
           <Text style={paragraph}>
             Visit our website and start empowering the world to develop technology through collective knowledge.
           </Text>
+          <Text style={paragraph}>
+          We’re thrilled to have you join our community of programmers. Here’s how you can get started:
+          </Text>
           <Button style={button} href={`${url}/dashboard`}>
-            Make your first question
+            Ask Questions
           </Button>
+          <Button style={button} href={`${url}/dashboard`}>
+            Answer Questions
+          </Button>
+
+          <Button style={button} href={`${url}/dashboard`}>
+            Explore Topics
+          </Button>
+          <Text style={paragraph}>
+          Need help? Contact us at devvitnuclio@gmail.com  or check out our help section</Text>
+          <Text style={paragraph}>
+         Happy coding!
+         </Text>
+         <Text style={paragraph}>
+         The Devvit Team
+         </Text>
         </Section>
       </Container>
     </Body>
@@ -25,15 +43,21 @@ export const WelcomeEmail = ({ url }) => (
 export default WelcomeEmail;
 
 const main = {
-  backgroundColor: '#f6f9fc',
+  backgroundColor: '#000000',
   fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif',
 };
 
 const container = {
-  backgroundColor: '#ffffff',
+  backgroundColor: '#000000',
   margin: '0 auto',
   padding: '20px 0 48px',
   marginBottom: '64px',
+};
+
+const title = {
+  color: '#fff',
+  fontSize: "35px",
+  marginBottom: '20px',
 };
 
 const box = {
@@ -46,9 +70,12 @@ const hr = {
 };
 
 const paragraph = {
-  color: '#525f7f',
+  color: '#fff',
   fontSize: '18px',
   lineHeight: '24px',
+  margin: '15px 15px', 
+  lineHeight: '28px', 
+  margin: '20px 0', 
 };
 
 const button = {
@@ -62,4 +89,7 @@ const button = {
   display: 'block',
   width: '100%',
   padding: '10px',
+  marginBottom: '17px',
+  
 };
+
