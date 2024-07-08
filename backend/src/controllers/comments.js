5037f0181850a156972105d446c70ddd5ab1ed78
Alex Besse Donato <alexbessedonato03@gmail.com>
2024-07-08 20:29:36 +0200

    merge de emailsending

--- backend/src/controllers/comments.js
+++ backend/src/controllers/comments.js
@@ -12,15 +12,15 @@ const createComment = async (req, res) => {
 
     await newComment.save();
     console.log('Comment saved successfully', newComment);
-    
-    // const questionData = await Question.findById(question).populate('user'); // Assuming the question has a 'user' field referring to its owner
-    // if (questionData) {
-    //   const ownerEmail = questionData.user.email;
-    //   const ownerUsername = questionData.user.name;
-    //   const commenterName = user.name;
 
-    //   await sendCommentNotificationEmail(ownerEmail, ownerUsername, commenterName);
-    // }
+    const questionData = await Question.findById(question).populate('user'); // Assuming the question has a 'user' field referring to its owner
+    if (questionData) {
+      const ownerEmail = questionData.user.email;
+      const ownerUsername = questionData.user.username;
+      const commenterName = user.username;
+
+      await sendCommentNotificationEmail(ownerEmail, ownerUsername, commenterName);
+    }
 
     res.status(201).json(newComment);
   } catch (error) {
