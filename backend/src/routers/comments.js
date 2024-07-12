2ad9e1d0cbef6b821500d76aaac96bcd7f2f45e4
kovvik23 <08kovacs.v@gmail.com>
2024-07-12 10:13:49 +0200

    Add commentsbyId route

--- backend/src/routers/comments.js
+++ backend/src/routers/comments.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createComment, editComment, deleteComment } = require('../controllers/comments');
+const { createComment, editComment, deleteComment, getCommentsByQuestionId } = require('../controllers/comments');
 const validateCommentInput = require('../middlewares/comments');
 const { jwtMiddleware } = require('../middlewares/jwt');
 
@@ -10,5 +10,6 @@ router.use(jwtMiddleware);
 router.post('/comments', validateCommentInput, createComment);
 router.put('/comments/:id', validateCommentInput, editComment);
 router.delete('/comments/:id', deleteComment);
+router.get('/comments/:questionId', getCommentsByQuestionId);
 
 module.exports = router;
