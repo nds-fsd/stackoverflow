9b6948a3064d5d1a7a447607feb71925fb8360d5
kovvik23 <08kovacs.v@gmail.com>
2024-07-15 20:35:37 +0200

    Add JWT middleware to likes

--- backend/src/routers/like.js
+++ backend/src/routers/like.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { createLike, deleteLike, getLikeCountByCommentId, getLikesByUserId } = require('../controllers/like');
 const router = express.Router();
+const { jwtMiddleware } = require('../middlewares/jwt');
 
-router.post('/likes', createLike);
-router.delete('/likes', deleteLike); // Add this line
+router.post('/likes', jwtMiddleware, createLike);
+router.delete('/likes', jwtMiddleware, deleteLike);
 router.get('/likes/:commentId', getLikeCountByCommentId);
 router.get('/user-likes/:userId', getLikesByUserId);
 
